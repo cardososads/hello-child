2feacfd9d3084759ee5033f71bbc05ad10824e0c
Wesley Alves Cardoso <66920722+cardososads@users.noreply.github.com>
2024-07-10 03:36:58 -0400

    saidas 4

--- functions.php
+++ functions.php
@@ -17,16 +17,38 @@ require_once get_stylesheet_directory() . '/class-numerology-calculator.php';
 
 new Theme_Setup();
 
+function display_jetengine_data_shortcode() {
+    $jet_engine_options = new JetEngine_Options();
+
+    $introductions = $jet_engine_options->get_introductions();
+    $all_repeaters = $jet_engine_options->get_all_repeaters();
+
+    ob_start();
+
+    echo '<pre>';
+    echo "Introduções:\n";
+    print_r($introductions);
+
+    echo "\nRepeaters:\n";
+    print_r($all_repeaters);
+    echo '</pre>';
+
+    return ob_get_clean();
+}
+
+add_shortcode('display_jetengine_data', 'display_jetengine_data_shortcode');
+
 // Hook para processar o envio dos formulários
 add_action('elementor_pro/forms/new_record', function ($record, $handler) {
-    // Certifique-se de que o handler é o Elementor Forms
-    if ('elementor_pro' !== $handler->get_id()) {
-        return;
-    }
+    // Verifique qual formulário foi enviado
+    $form_name = $record->get_form_settings('form_name');
 
     // Obtenha os dados do formulário
-    $form_name = $record->get_form_settings('form_name');
-    $fields = $record->get('fields');
+    $raw_fields = $record->get('fields');
+    $fields = [];
+    foreach ($raw_fields as $id => $field) {
+        $fields[$id] = $field['value'];
+    }
 
     // Instancia a classe de cálculo
     $calculator = new NumerologyCalculator();
@@ -35,31 +57,20 @@ add_action('elementor_pro/forms/new_record', function ($record, $handler) {
     switch ($form_name) {
         case 'Form1':
             // Realiza o cálculo do número de destino
-            $data = [];
-            foreach ($fields as $id => $field) {
-                $data[$id] = $field['value'];
-            }
-            $data['destiny_number'] = $calculator->calculateDestinyNumber($data['birth_date']);
-            set_transient('form1_submission_data', $data, 60 * 60); // Armazena por 1 hora
+            $fields['destiny_number'] = $calculator->calculateDestinyNumber($fields['birth_date']);
+            set_transient('form1_submission_data', $fields, 60 * 60); // Armazena por 1 hora
             break;
         case 'Form2':
             // Realiza o cálculo do número de expressão
-            $data = [];
-            foreach ($fields as $id => $field) {
-                $data[$id] = $field['value'];
-            }
-            $data['expression_number'] = $calculator->calculateExpressionNumber($data['full_name']);
-            set_transient('form2_submission_data', $data, 60 * 60); // Armazena por 1 hora
+            $fields['expression_number'] = $calculator->calculateExpressionNumber($fields['full_name']);
+            set_transient('form2_submission_data', $fields, 60 * 60); // Armazena por 1 hora
             break;
         case 'Form3':
             // Armazena os dados do formulário 3
-            $data = [];
-            foreach ($fields as $id => $field) {
-                $data[$id] = $field['value'];
-            }
-            set_transient('form3_submission_data', $data, 60 * 60); // Armazena por 1 hora
+            set_transient('form3_submission_data', $fields, 60 * 60); // Armazena por 1 hora
             break;
     }
+
 }, 10, 2);
 
 // Shortcode para exibir os resultados dos formulários
@@ -67,21 +78,10 @@ function show_form_results($atts) {
     $atts = shortcode_atts(['form' => ''], $atts, 'show_form_results');
     $form = $atts['form'];
     $data = get_transient($form . '_submission_data');
-    $jet_engine_options = new JetEngine_Options();
-
-    $introductions = $jet_engine_options->get_introductions();
-    $all_repeaters = $jet_engine_options->get_all_repeaters();
 
     if (!$data) {
         return '<p>Nenhum dado encontrado.</p>';
     }
-    echo '<pre>';
-    echo "Introduções:\n";
-    print_r($introductions);
-
-    echo "\nRepeaters:\n";
-    print_r($all_repeaters['_numeros_destino_516'][$data['destiny_number']]);
-    echo '</pre>';
 
     ob_start();
     echo '<pre>';
