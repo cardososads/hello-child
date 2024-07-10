9e4d094a240798159b8b8a770c4ddd1704def757
Wesley Alves Cardoso <66920722+cardososads@users.noreply.github.com>
2024-07-10 01:17:39 -0400

    add _

--- functions.php
+++ functions.php
@@ -9,22 +9,8 @@ require_once get_stylesheet_directory() . '/class-jetengine-options.php';
 new Theme_Setup();
 
 function display_jetengine_data_shortcode() {
-    $jet_engine_options = new JetEngine_Options('_audios');
-
-    $introductions = $jet_engine_options->get_introductions();
-    $all_repeaters = $jet_engine_options->get_all_repeaters();
-
-    ob_start();
-
-    echo '<pre>';
-    echo "Introduções:\n";
-    print_r($jet_engine_options);
-
-    echo "\nRepeaters:\n";
-    print_r($all_repeaters);
-    echo '</pre>';
-
-    return ob_get_clean();
+    $audios = get_field('_audios');
+    var_dump($audios);
 }
 
 add_shortcode('display_jetengine_data', 'display_jetengine_data_shortcode');
