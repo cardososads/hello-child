d2714c86d9fd180584e317e6b2593f66c80c9cee
Wesley Alves Cardoso <66920722+cardososads@users.noreply.github.com>
2024-07-10 03:12:58 -0400

    retornos 2

--- functions.php
+++ functions.php
@@ -17,6 +17,86 @@ require_once get_stylesheet_directory() . '/class-numerology-calculator.php';
 
 new Theme_Setup();
 
+// Hook para processar o envio dos formulários
+add_action('elementor_pro/forms/new_record', function ($record, $handler) {
+    // Certifique-se de que o handler é o Elementor Forms
+    if ('elementor_pro' !== $handler->get_id()) {
+        return;
+    }
+
+    // Obtenha os dados do formulário
+    $form_name = $record->get_form_settings('form_name');
+    $fields = $record->get('fields');
+
+    // Instancia a classe de cálculo
+    $calculator = new NumerologyCalculator();
+
+    // Armazena os dados do formulário usando transients para acesso global
+    $data = [];
+    foreach ($fields as $id => $field) {
+        $data[$id] = $field['value'];
+    }
+
+    switch ($form_name) {
+        case 'Form1':
+            // Realiza o cálculo do número de destino
+            $data['destiny_number'] = $calculator->calculateDestinyNumber($data['birth_date']);
+            set_transient('form1_submission_data', $data, 60 * 60); // Armazena por 1 hora
+            break;
+
+        case 'Form2':
+            // Realiza o cálculo do número de expressão
+            $data['expression_number'] = $calculator->calculateExpressionNumber($data['full_name']);
+            set_transient('form2_submission_data', $data, 60 * 60); // Armazena por 1 hora
+            break;
+
+        case 'Form3':
+            // Armazena os dados do formulário 3
+            set_transient('form3_submission_data', $data, 60 * 60); // Armazena por 1 hora
+            break;
+    }
+}, 10, 2);
+
+// Função para exibir os resultados dos formulários
+function show_form_results($atts) {
+    $atts = shortcode_atts(['form' => ''], $atts, 'show_form_results');
+    $form = $atts['form'];
+    $data = get_transient($form . '_submission_data');
+
+    if (!$data) {
+        return '<p>Nenhum dado encontrado.</p>';
+    }
+
+    // Filtrar e exibir áudios de introdução e áudio do número de destino
+    if ($form === 'form1_submission_data') {
+        $jet_engine_options = new JetEngine_Options();
+        $introductions = $jet_engine_options->get_introductions();
+        $all_repeaters = $jet_engine_options->get_all_repeaters();
+
+        $destiny_number = $data['destiny_number'];
+        $destiny_audio = $all_repeaters['_numeros_destino_516'][$destiny_number] ?? 'Nenhum áudio encontrado para o número de destino.';
+
+        ob_start();
+        echo '<pre>';
+        echo "Áudios de Introdução:\n";
+        print_r($introductions);
+
+        echo "\nÁudio do Número de Destino:\n";
+        print_r($destiny_audio);
+        echo '</pre>';
+        return ob_get_clean();
+    }
+
+    // Exibir os dados do formulário conforme recebido
+    ob_start();
+    echo '<pre>';
+    print_r($data);
+    echo '</pre>';
+    return ob_get_clean();
+}
+add_shortcode('show_form_results', 'show_form_results');
+
+// Shortcode para exibir os dados do JetEngine
 function display_jetengine_data_shortcode() {
     $jet_engine_options = new JetEngine_Options();
 
@@ -37,66 +117,3 @@ function display_jetengine_data_shortcode() {
 }
 
 add_shortcode('display_jetengine_data', 'display_jetengine_data_shortcode');
-
-// Hook para processar o envio dos formulários
-add_action('elementor_pro/forms/new_record', function ($record, $handler) {
-    // Certifique-se de que o handler é o Elementor Forms
-    if ('elementor_pro' !== $handler->get_id()) {
-        return;
-    }
-
-    // Obtenha os dados do formulário
-    $form_name = $record->get_form_settings('form_name');
-    $fields = $record->get('fields');
-
-    // Instancia a classe de cálculo
-    $calculator = new NumerologyCalculator();
-
-    // Armazena os dados do formulário usando transients para acesso global
-    switch ($form_name) {
-        case 'Form1':
-            // Realiza o cálculo do número de destino
-            $data = [];
-            foreach ($fields as $id => $field) {
-                $data[$id] = $field['value'];
-            }
-            $data['destiny_number'] = $calculator->calculateDestinyNumber($data['birth_date']);
-            set_transient('form1_submission_data', $data, 60 * 60); // Armazena por 1 hora
-            break;
-        case 'Form2':
-            // Realiza o cálculo do número de expressão
-            $data = [];
-            foreach ($fields as $id => $field) {
-                $data[$id] = $field['value'];
-            }
-            $data['expression_number'] = $calculator->calculateExpressionNumber($data['full_name']);
-            set_transient('form2_submission_data', $data, 60 * 60); // Armazena por 1 hora
-            break;
-        case 'Form3':
-            // Armazena os dados do formulário 3
-            $data = [];
-            foreach ($fields as $id => $field) {
-                $data[$id] = $field['value'];
-            }
-            set_transient('form3_submission_data', $data, 60 * 60); // Armazena por 1 hora
-            break;
-    }
-}, 10, 2);
-
-// Shortcode para exibir os resultados dos formulários
-function show_form_results($atts) {
-    $atts = shortcode_atts(['form' => ''], $atts, 'show_form_results');
-    $form = $atts['form'];
-    $data = get_transient($form . '_submission_data');
-
-    if (!$data) {
-        return '<p>Nenhum dado encontrado.</p>';
-    }
-
-    ob_start();
-    echo '<pre>';
-    print_r($data);
-    echo '</pre>';
-    return ob_get_clean();
-}
-add_shortcode('show_form_results', 'show_form_results');
