3d72669ca052e8cddb75f78f573282a2d2e54bf1
Wesley Alves Cardoso <66920722+cardososads@users.noreply.github.com>
2024-07-10 01:19:21 -0400

    add _

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ require_once get_stylesheet_directory() . '/class-jetengine-options.php';
 new Theme_Setup();
 
 function display_jetengine_data_shortcode() {
-    $audios = get_field('_audios');
+    $audios = get_option('_audios');
     var_dump($audios);
 }
 
